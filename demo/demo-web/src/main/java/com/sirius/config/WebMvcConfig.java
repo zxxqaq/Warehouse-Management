e1a6f2b75a085176c87b596f356f673a945f2243
Jingxuan Sun <89790849+PirxTion@users.noreply.github.com>
2024-01-16 14:42:32 +0800

    delete /api

--- demo/demo-web/src/main/java/com/sirius/config/WebMvcConfig.java
+++ demo/demo-web/src/main/java/com/sirius/config/WebMvcConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
+        registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedHeaders("*")
                 .allowedMethods("GET", "POST");
